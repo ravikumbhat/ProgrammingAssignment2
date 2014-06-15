af63ca858d874576816a6f36f0efc359d707cf2f
ravi <ravikumbhat@gmail.com>
2014-06-15 12:52:54 +0530

    Ver0.1
    First commit of cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+
+## -------------------------------makeCacheMatrix------------------------##
+## makeCacheMatrix function to cache inverse of a square matrix 
+##supplied as an argument by calling cacheSolve function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  #check if cacheVariable is defined
+  if(!exists("invMatrix"))
+  {
+    invMatrix<<-NULL
+    #print("invMatrix value doesn't exist in memory. def invMatrix with NULL value")
+  }
+  
+  
+  cacheSolve(x)
+  
+  print(invMatrix)
+  
 }
 
-
-## Write a short comment describing this function
+##----------------------------cachSolve----------------------------------------##
+## cacheSolve function to check whether matrix is inversible & 
+## then calculateinverse of the matrix 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  #check if cacheVariable is not null Value. 
+  #if cacheVariable is null, compute the matrix inverse
+  #also store matrix inverse values in another variable for future comparison
+  if(is.null(invMatrix))
+  {
+    invMatrix<<-solve(x)
+    staticInvMatrix<<-invMatrix
+    #print("matrix is currently null. computing inverse of matrix now.")
+    
+  }
+  else
+  {
+    ## if cacheVariable exists, check if the original values are maintained
+    ## return the cacheVariable if the condition is satisfied
+    if(!identical(invMatrix,staticInvMatrix)) 
+    {
+      ##solving the matrix again as the values are changed
+      invMatrix<<-solve(x)
+      #print("values changed in the matrix. hence recomputed")
+      
+    }
+  }
+  return(invMatrix)
+  ## Return a matrix that is the inverse of 'x'
 }
